841fdf010e872e0f521b7e58edd18688c7c75773
bytorbix <bytorbix@gmail.com>
2026-08-03 15:29:58 +0300

    Add RecordReaderFactory for file-type-based reader selection

---
+++ TelemetrySimulator/Ingestion/RecordReaderFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace TelemetrySimulator.Ingestion
+{
+    public enum FileType
+    {
+        Csv
+        // Json later
+    }
+
+    public class RecordReaderFactory
+    {
+        public IRawRecordReader Create(FileType fileType)
+        {
+            return fileType switch
+            {
+                FileType.Csv => new CsvRecordReader(),
+                _ => throw new ArgumentOutOfRangeException(nameof(fileType), $"Unsupported file type: {fileType}")
+            };
+        }
+    }
+}
