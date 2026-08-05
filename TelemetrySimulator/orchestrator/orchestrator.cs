1204e9f44fff4bf106098131bdba5aed185a8c8d
bytor <bytor@DESKTOP-U1V5ORO>
2026-08-05 13:54:09 +0300

    implemented orchestrator packet emit simulation

--- TelemetrySimulator/orchestrator/orchestrator.cs
+++ TelemetrySimulator/orchestrator/orchestrator.cs
@@ -1,6 +1,37 @@
+using System.Net.Sockets;
 using TelemetrySimulator.Icd;
+using TelemetrySimulator.Mapping;
+using TelemetrySimulator.Resolving;
 
-public class Orchestrator
+public class Orchestrator(IcdDocument icd, MappingConfig mapping)
 {
+    const string CORRELATOR_PARAM_NAME = "correlator";
+
+    Encoder _encoder = new();
+    Resolver _resolver = new();
+
+    public async Task SimulateAsync(List<Dictionary<string, string>> rawRecords, UdpClient socket, int intervalMs, int startIndex = 0, int? packetsCount = null)
+    {
+        IEnumerable<Dictionary<string, string>> rows = rawRecords.Skip(startIndex).Take(packetsCount ?? rawRecords.Count); // cut rows to desired index and amount
+
+        int correlatorRange = 1 << icd.GetField(CORRELATOR_PARAM_NAME).Size;
+        int correlatorValue = 0;
+
+        
+
+        foreach (Dictionary<string, string> record in rows)
+        {
+            // resolve and map values from raw record to ICD identifiers
+            Dictionary<string, double> resolvedValues = _resolver.Resolve(record, mapping);
+
+            byte[] frame = _encoder.BuildFrame(icd, resolvedValues, correlatorValue);
+
+            await socket.SendAsync(frame, frame.Length);
+
+            correlatorValue = (correlatorValue + 1) % correlatorRange; // inc correaltor 
+
+            await Task.Delay(intervalMs); // fixed interval ms between packets
+        }
+    }
 
 }
