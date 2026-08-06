d2f97bdf30bb321e4636badc56003cd9fa63d478
bytor <bytor@DESKTOP-U1V5ORO>
2026-08-06 16:06:22 +0300

    Revert "Implemented Controllers and seperated them to Controllers+Services"
    This reverts commit 5e3df4eae51f208e831da6577b5ac14c222089a1.

--- TelemetrySimulator/Program.cs
+++ TelemetrySimulator/Program.cs
@@ -1,6 +1,5 @@
 using TelemetrySimulator.Icd;
 using TelemetrySimulator.Resolving;
-using TelemetrySimulator.Services;
 using TelemetrySimulator.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,15 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<UploadStore>();
 
-// Services
-builder.Services.AddScoped<UploadService>();
-builder.Services.AddScoped<SimulationService>();
-
 // singleton instances
 builder.Services.AddSingleton<Encoder>();
 builder.Services.AddSingleton<Resolver>();
 builder.Services.AddSingleton<Orchestrator>();
-builder.Services.AddSingleton<SimulationRegistry>();
 
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
